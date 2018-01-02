099b38f93f03db9b65548a3395004b312eafee17
Sebastien Cadorette <cado.94@me.com>
2018-01-02 17:28:05 -0500

    SC : About page dev.

--- js/about/about.js
+++ js/about/about.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
+    function Project(){
+        this.title = "";
+        this.description = "";
+        this.projectDetailsTitle = "";
+        this.details = "";
+        this.pictureURL = "";
+    }
+
+    // Get the current shown project informations.
+    var currentShownProject = new Project();
+    currentShownProject.title = document.getElementById("projectTitle").innerHTML;
+    currentShownProject.description = document.getElementById("projectDescription").innerHTML;
+
+    var relatedProject1 = new Project();
+    relatedProject1.title = "Related project 1";
+
+
     $("#relatedProject1").click(function(){
-        console.log("Hello there.");
+
+        
+        document.getElementById("projectTitle").innerHTML = relatedProject1.title;
     });
 });
+
